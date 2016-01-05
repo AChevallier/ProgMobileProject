227645ebb54b8824e7662c473360b243b5578a09
svandycke <svandycke@gmail.com>
2016-01-05 15:37:33 +0100

    GET JSON

---
+++ app/src/main/java/com/example/steve/quefaireici/GetAsync.java
@@ -0,0 +1,94 @@
+package com.example.steve.quefaireici;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+/**
+ * Created by Steve on 05/01/2016.
+ */
+public class GetAsync extends AsyncTask<String, String, JSONObject> {
+    JSONParser jsonParser = new JSONParser();
+
+    private ProgressDialog pDialog;
+
+    private static final String LOGIN_URL = "http://stevevandycke.free.fr/QueFaireIci/activites.json";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    private Context context;
+
+    public GetAsync(Context context){
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        pDialog = new ProgressDialog(context);
+        pDialog.setMessage("Attempting login...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(true);
+        pDialog.show();
+    }
+
+    @Override
+    protected JSONObject doInBackground(String... args) {
+
+        try {
+
+            HashMap<String, String> params = new HashMap<>();
+
+            Log.d("request", "starting");
+
+            JSONObject json = jsonParser.makeHttpRequest(
+                    LOGIN_URL, "GET", params);
+
+            if (json != null) {
+                Log.d("JSON result", json.toString());
+
+                return json;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    protected void onPostExecute(JSONObject json) {
+
+        int success = 0;
+        String message = "";
+
+        if (pDialog != null && pDialog.isShowing()) {
+            pDialog.dismiss();
+        }
+
+        if (json != null) {
+            Toast.makeText(context, json.toString(),
+                    Toast.LENGTH_LONG).show();
+
+            try {
+                success = json.getInt(TAG_SUCCESS);
+                message = json.getString(TAG_MESSAGE);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (success == 1) {
+            Log.d("Success!", message);
+        }else{
+            Log.d("Failure", message);
+        }
+    }
+}
--- app/src/main/java/com/example/steve/quefaireici/MainActivity.java
+++ app/src/main/java/com/example/steve/quefaireici/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.steve.quefaireici;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GetAsync async = new GetAsync(this);
+        async.execute();
+
     }
 }
+
+
+
